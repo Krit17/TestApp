eb48bba3c26133b72dda4696cf78ad8e127b76a8
Krithika <krithikaa1705@gmail.com>
2018-02-14 09:54:08 +0530

    First

---
+++ app/src/main/java/com/example/android/testapp/StartScreen.java
@@ -0,0 +1,22 @@
+package com.example.android.testapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+
+Button start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+    void start(View view){
+        start = (Button)view;
+        Intent intent = new Intent(StartScreen.this,Log)
+    }
+
+}
